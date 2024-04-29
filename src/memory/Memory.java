1a1199dccb69105745dce19e1d9ae5bd5f15df41
Oussama <wildric.auric@gmail.com>
2024-04-29 22:28:30 +0200

    replaced stack with vector, adapted memory for functions

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -1,45 +1,66 @@
 package memory;
 
+import frontend.FunctionCallException;
 import frontend.VariableException;
 
 import java.util.HashMap;
-import java.util.Stack;
+//Im using vectors because iteration isn't from back in stack, check doc
+import java.util.Vector;
+
+
+import ast.ASTfunction;
+
 
 public class Memory {
-    private static Stack< HashMap<String, Object> >  data = new Stack<HashMap<String,Object>>();    //Ouss. Bad memory, too much cache issue, but it is java lol
+    public static Vector< HashMap<String, Object> >       data = new Vector<HashMap<String,Object>>();    //Ouss. Bad memory, too much cache issue, but it is java lol
+    private static HashMap<String, ASTfunction>      funcData = new HashMap<String, ASTfunction>();
 
     public static void init() {
         //Ouss.Initialize main scope
-        data.push(new HashMap<String,Object>());
+        data.add(new HashMap<String,Object>());
     }
 
     public static void pushScope() {
-        data.push(new HashMap<String,Object>()); 
+        data.add(new HashMap<String,Object>()); 
     }
 
     public static void popScope() {
-        data.pop();
+        data.remove(data.size()-1);
     }
 
     public static void pushVar(String id, Object value) {
-
-        for (HashMap<String, Object> scope : data) {
-            Object val = scope.get(id);
+        for (int i = data.size()-1; i > -1; --i) {
+            Object val = data.get(i).get(id);
             if (val != null) {
-                scope.put(id, value);
+                data.get(i).put(id, value);
                 return;
-            }
+            }  
         }
-        data.peek().put(id, value);
+        data.lastElement().put(id, value);
+    }
+
+    //Force pushing a variable, used for functions
+    public static void pushVarForce(String id, Object value) {
+        data.lastElement().put(id,value);
     }
 
     public static Object getVar(String id) {
-
-        for (HashMap<String, Object> scope : data) {
-            Object val = scope.get(id);
+        for (int i = data.size()-1; i > -1; --i) {
+            Object val = data.get(i).get(id);
             if (val != null)
                 return val;
         }
         throw new VariableException("Unknown variable : " + id);
+    }
+
+    public static void pushFunc(ASTfunction func) {
+        funcData.put(func.getID(), func);
+    }
+
+    public static ASTfunction getFunc(String id) {
+        ASTfunction f = funcData.get(id);
+        if (f != null) 
+            return f;
+        throw new FunctionCallException("Unknown function: " + id);
     }
 }
